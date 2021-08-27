b68d8da4e970054a769300eae70da990e7f9cfd4
sonin <1457065857@qq.com>
2021-08-27 16:34:32 +0800

    补充swagger模块

--- smart-cloud-gateway/src/main/java/com/scframework/smartcloudgateway/swagger/component/GatewaySwaggerResourceProvider.java
+++ smart-cloud-gateway/src/main/java/com/scframework/smartcloudgateway/swagger/component/GatewaySwaggerResourceProvider.java
@@ -1,4 +1,4 @@
-package com.scframework.smartcloudgateway.swagger;
+package com.scframework.smartcloudgateway.swagger.component;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
